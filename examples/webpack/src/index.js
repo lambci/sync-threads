d1005c5051202fbf1571520344799c9519a096fd
Michael Hart <michael.hart.au@gmail.com>
2020-10-31 20:33:49 -0400

    Oops, copy example from README

--- examples/webpack/src/index.js
+++ examples/webpack/src/index.js
@@ -1,15 +1,15 @@
 const { createSyncFn } = require('sync-threads')
 
+// Create our synchronous function
 const getSsmSecretSync = createSyncFn('./worker.js')
 
-// Happens at init (require) time
+// Call it at init (require) time, no async needed!
 const secret = getSsmSecretSync('/my-secret')
 
+// Logged at init time
 console.log({ secret, source: 'init' })
 
 exports.handler = async () => {
-  // Happens every invoke
-  const secret = getSsmSecretSync('/my-secret')
-
+  // This value can be used in our handler without needing to resolve anything async
   console.log({ secret, source: 'handler' })
 }
