d630f2423831951534bdffa12d9cef805f5306bc
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2024-01-03 10:41:49 +0100

    Generate latest types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,12 @@
-export function createSyncFn(filename: string, bufferSize?: number): (inputData?: {}) => any;
-export function runAsWorker(workerAsyncFn: (inputData: any) => Promise<any>): Promise<void>;
+export type CreateFnOptionsRaw = {
+    bufferSize?: number | undefined;
+    maxBufferSize?: number | undefined;
+    timeout?: number | undefined;
+};
+export type CreateFnOptions = {
+    bufferSize: number;
+    maxBufferSize: number;
+    timeout?: number | undefined;
+};
+export function createSyncFn(filename: string, bufferSizeOrOptions?: number | CreateFnOptionsRaw | undefined): (...args: any) => any;
+export function runAsWorker(workerAsyncFn: (...inputData: any) => Promise<any>): Promise<void>;
